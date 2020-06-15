e23f5f7371dd19f6cefd0cbf61a606222fbff9bd
Mathvivas <61150900+Mathvivas@users.noreply.github.com>
2020-06-15 16:13:37 -0300

    Instanciação (Sistema)
    - Instanciação da Classe Sistema na Main.
    - Modificações no Design do Menu da Pizzaria

--- src/br/maua/Classes/Sistema.java
+++ src/br/maua/Classes/Sistema.java
@@ -4,10 +4,10 @@ public class Sistema {
 
     public void run() {
         System.out.println("| ____________ | Pizzaria o Rato que Ri | ____________ |");
-        System.out.println("|\t1 - Nova Venda");
-        System.out.println("|\t2 - Verificar Pedidos");
-        System.out.println("|\t3 - Alterar Pedidos");
-        System.out.println("|\t0 - Sair");
-        System.out.println("|_____________________________________________________");
+        System.out.println("|\t\t1 - Nova Venda                                 |");
+        System.out.println("|\t\t2 - Verificar Pedidos                          |");
+        System.out.println("|\t\t3 - Alterar Pedidos                            |");
+        System.out.println("|\t\t0 - Sair                                       |");
+        System.out.println("|______________________________________________________|");
     }
 }
--- src/br/maua/Main.java
+++ src/br/maua/Main.java
@@ -1,8 +1,12 @@
 package br.maua;
 
+import br.maua.Classes.Sistema;
+
 public class Main {
 
     public static void main(String[] args) {
+        Sistema sistema = new Sistema();
 
+        sistema.run();
     }
 }
