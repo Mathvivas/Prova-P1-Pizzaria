42f070774809330b075653ae2bb3ff9c41ff6b70
Mathvivas <61150900+Mathvivas@users.noreply.github.com>
2020-06-15 16:01:48 -0300

    Método run
    - Criação do Método run na Classe Sistema.
    - Menu da Pizzaria.

--- src/br/maua/Classes/Sistema.java
+++ src/br/maua/Classes/Sistema.java
@@ -1,4 +1,13 @@
 package br.maua.Classes;
 
 public class Sistema {
+
+    public void run() {
+        System.out.println("| ____________ | Pizzaria o Rato que Ri | ____________ |");
+        System.out.println("|\t1 - Nova Venda");
+        System.out.println("|\t2 - Verificar Pedidos");
+        System.out.println("|\t3 - Alterar Pedidos");
+        System.out.println("|\t0 - Sair");
+        System.out.println("|_____________________________________________________");
+    }
 }
