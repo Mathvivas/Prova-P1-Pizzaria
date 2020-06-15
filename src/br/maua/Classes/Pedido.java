d9b072e7543545e79afa2d293e7715a3d6261336
Mathvivas <61150900+Mathvivas@users.noreply.github.com>
2020-06-15 15:24:42 -0300

    Update Classe Pedido
    - Adicionadas as formas de pagamento e os estados do pedido.
    - Criado o Construtor da Classe Pedido.

--- src/br/maua/Classes/Pedido.java
+++ src/br/maua/Classes/Pedido.java
@@ -1,7 +1,21 @@
 package br.maua.Classes;
 
+import br.maua.Enumeracoes.Estado;
+import br.maua.Enumeracoes.FormaDePagamento;
+
 public class Pedido {
     private String ID;
     private String descricao;
     private double valor;
+    private FormaDePagamento formaDePagamento;
+    private Estado estado;
+
+    // Construtor
+    public Pedido(String ID, String descricao, double valor, FormaDePagamento formaDePagamento, Estado estado) {
+        this.ID = ID;
+        this.descricao = descricao;
+        this.valor = valor;
+        this.formaDePagamento = formaDePagamento;
+        this.estado = estado;
+    }
 }
