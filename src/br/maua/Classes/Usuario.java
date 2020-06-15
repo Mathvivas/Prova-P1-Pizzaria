9cdf7200afa88edbd517057e43a22c60ddaba489
Mathvivas <61150900+Mathvivas@users.noreply.github.com>
2020-06-15 13:37:25 -0300

    Construtor da Classe
    Construtor da Classe Usuário

--- src/br/maua/Classes/Usuario.java
+++ src/br/maua/Classes/Usuario.java
@@ -1,4 +1,16 @@
 package br.maua.Classes;
 
 public class Usuario {
+    private String nome;
+    private String email;
+    private String senha;
+
+    // Construtor da Classe
+    public void Usuario(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+
 }
