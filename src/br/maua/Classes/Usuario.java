f8d81041ef29936ad464e7de5a33ce758566fdf6
Mathvivas <61150900+Mathvivas@users.noreply.github.com>
2020-06-21 14:00:01 -0300

    Javadoc - Usuário
    - Criação do javadoc da Classe Usuário.

--- src/br/maua/Classes/Usuario.java
+++ src/br/maua/Classes/Usuario.java
@@ -1,9 +1,23 @@
 package br.maua.Classes;
 
+/**
+ * Classe concreta que representa o funcionário (usuário).
+ *
+ * @author Matheus Lopes Vivas - mathvivas@hotmail.com
+ * @since 16/06/2020
+ * @version 1.0
+ */
+
 public class Usuario {
     private String nome;
     private String email;
     private String senha;
+
+    /**
+     * @param nome Nome do funcionário da Pizzaria.
+     * @param email Email do funcionário da Pizzaria.
+     * @param senha Senha do funcionário da Pizzaria.
+     */
 
     // Construtor da Classe
     public Usuario(String nome, String email, String senha) {
@@ -11,6 +25,10 @@ public class Usuario {
         this.email = email;
         this.senha = senha;
     }
+
+    /**
+     * @return Getter para a senha do funcionário (usuário).
+     */
 
     public String getSenha() {
         return senha;
