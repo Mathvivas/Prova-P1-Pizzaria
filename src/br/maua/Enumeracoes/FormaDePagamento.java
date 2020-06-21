9bca69eb1496c932d45710fd205ac1ffb993547e
Mathvivas <61150900+Mathvivas@users.noreply.github.com>
2020-06-21 13:52:05 -0300

    Javadoc - FormaDePagamento
    - Criação do javadoc na Enumeração FormaDePagamento.

--- src/br/maua/Enumeracoes/FormaDePagamento.java
+++ src/br/maua/Enumeracoes/FormaDePagamento.java
@@ -1,5 +1,12 @@
 package br.maua.Enumeracoes;
 
+/**
+ * Enumeração que define as formas de pagamento disponíveis.
+ *
+ * @author Matheus Lopes Vivas - mathvivas@hotmail.com
+ * @since 16/06/2020
+ * @version 1.0
+ */
 public enum FormaDePagamento {
     DINHEIRO, DEBITO, CREDITO, VALE_ALIMENTACAO, VALE_REFEICAO;
 }
