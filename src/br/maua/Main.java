eb6fb1e7e99b8b9b077cc1e672e7a456d4a72c09
Mathvivas <61150900+Mathvivas@users.noreply.github.com>
2020-06-21 13:22:40 -0300

    Javadoc - Main
    - Criação do javadoc na Classe Main

--- src/br/maua/Main.java
+++ src/br/maua/Main.java
@@ -2,6 +2,14 @@ package br.maua;
 
 import br.maua.Classes.Sistema;
 
+/**
+ * Classe responsável por iniciar a aplicação, instanciando a Classe Sistema.
+ * Por meio dela, a aplicação é executada.
+ *
+ * @author Matheus Lopes Vivas - mathvivas@hotmail.com
+ * @since 15/06/2020
+ * @version 1.0
+ */
 public class Main {
 
     public static void main(String[] args) {
