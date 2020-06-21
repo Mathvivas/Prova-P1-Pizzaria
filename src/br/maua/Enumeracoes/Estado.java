85037c986289247ea059bf7058d551818bcffb11
Mathvivas <61150900+Mathvivas@users.noreply.github.com>
2020-06-21 13:32:37 -0300

    Javadoc - Estado
    - Criação do javadoc na Enumeração Estado.

--- src/br/maua/Enumeracoes/Estado.java
+++ src/br/maua/Enumeracoes/Estado.java
@@ -1,5 +1,13 @@
 package br.maua.Enumeracoes;
 
+/**
+ * Enumeração que define o estado da Pizza, desde o
+ * momento do pedido, até a entrega.
+ *
+ * @author Matheus Lopes Vivas - mathvivas@hotmail.com
+ * @since 16/06/2020
+ * @version 1.0
+ */
 public enum Estado {
     REALIZADO, PREPARACAO,
     SAIU_PARA_ENTREGA, ENTREGUE, DEVOLVIDO;
